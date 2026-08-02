713977e6b7d23e51ed1b767bec35d14bd26f0b1b
Siddharth Gupta <siddharthgupta1161@gmail.com>
2026-08-03 00:40:18 +0530

    Add TypeInTS.ts with variable declarations
    Initialized variables for drink, cups, channel name, and chai flavor with type annotations.

---
+++ TypeInTS.ts
@@ -0,0 +1,10 @@
+let drink = "chai"
+
+let cups = Math.random() > 0.5 ? 10 : 5
+
+let channelName = "Siddharth"
+channelName=12304
+
+let chaiFlavour:string = "Masala Chai"
+chaiFlavour="ginger"
+chaiFlavour=32 
