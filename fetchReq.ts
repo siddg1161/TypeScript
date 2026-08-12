654a0c022ea2dbbe8f81cdfe83ddf992ce207703
Siddharth Gupta <siddharthgupta1161@gmail.com>
2026-08-13 00:06:24 +0530

    Implement fetchData function to retrieve Todo data
    Fetch data from a placeholder API and handle errors.

---
+++ fetchReq.ts
@@ -0,0 +1,25 @@
+import axios from "axios"
+import type { AxiosResponse } from "axios";
+
+interface Todo {
+    userId:number;
+    id:number;
+    title:string;
+    completed:boolean
+}
+
+const fetchData = async () => {
+    try{
+       const response : AxiosResponse<Todo> = await axios.get(
+         "https://jsonplaceholder.typicode.com/todos/1"
+       )
+       if(!response.ok){
+        throw new Error(`HTTP error ${response.status}`)
+       }
+       const data:Todo= await response.json()
+    }
+    catch(error:any)
+    {
+      
+    }
+}
