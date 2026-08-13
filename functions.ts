d0e2d18e73ea161523973eff7c1fff6af654bae5
Siddharth Gupta <siddharthgupta1161@gmail.com>
2026-08-14 00:09:19 +0530

    Create functions.ts

---
+++ functions.ts
@@ -0,0 +1,18 @@
+function makeChai(type:string,cups:number)
+{
+    console.log(`Making ${cups} cups of ${type}`)
+}
+makeChai("Masala",2)
+
+
+function getChaiPrice(): number {
+    return 25
+}
+
+function logChai():void {
+    console.log("Chai is ready")
+    
+}
+
+
+//type?:string or type:sting="Masala" alwyas written at end in func params list
