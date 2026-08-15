6dd82d910573f039dbe5fda74f36478763c80990
Siddharth Gupta <siddharthgupta1161@gmail.com>
2026-08-16 00:54:53 +0530

    Add webReq.ts for fetching Todo data using Axios

---
+++ webReq.ts
@@ -0,0 +1,31 @@
+import axios,{AxiosResponse} from "axios"
+
+interface Todo {
+    userId:number;
+    id:number;
+    title:string;
+    completed:boolean
+}
+
+axios.get('https://example.com/data')
+.then(response => {
+    console.log(response.data)
+})
+
+const fetchData = async () => {
+    try{
+       const response : AxiosResponse<Todo> = await axios.get(
+         "https://jsonplaceholder.typicode.com/todos/1"
+       )
+       console.log("Todo",response.data) 
+    }
+    catch(error:any)
+    {
+        if(axios.isAxiosError(error)){
+            console.log("AxiosError",error.message);
+            if(error.response){
+                console.log(error.response.status)
+            }
+        }
+    }
+}
